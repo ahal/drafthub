26ef98be74a8f12e8c8998607e3a32ffeb089362
Andrew Halberstadt <halbersa@gmail.com>
2015-09-28 20:04:53 -0400

    Add current pick counter

--- drafthub/static/js/drafthub.js
+++ drafthub/static/js/drafthub.js
@@ -3,6 +3,8 @@ var drafted = [];
 function draft(player) {
     drafted.push(player);
     player.className = 'taken ' + player.className;
+
+    $('#current-pick').html(parseInt($('#current-pick').html(), 10) + 1);
 }
 
 function undo() {
@@ -11,6 +13,8 @@ function undo() {
     }
     var player = drafted.pop();
     player.className = player.className.replace('taken', '');
+
+    $('#current-pick').html(parseInt($('#current-pick').html(), 10) - 1);
 }
 
 $('#player-search').bind('input propertychange', function() {
