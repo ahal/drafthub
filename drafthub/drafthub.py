d0f7ce2c7452a39c3b7b1e111ff95f5f6cc80698
Andrew Halberstadt <halbersa@gmail.com>
2015-10-01 00:06:48 -0400

    Add ability to pull in live draft results and automatically update table

--- drafthub/drafthub.py
+++ drafthub/drafthub.py
@@ -1,6 +1,13 @@
 import csv
+import json
 import os
+import pdb
 
+from yahoo_oauth import OAuth1
+from myql import MYQL
+from myql.utils import prettyfy as dump
+
+from flask.json import jsonify
 from flask import (
     Flask,
     render_template,
@@ -8,17 +15,73 @@ from flask import (
 )
 
 here = os.path.abspath(os.path.dirname(__file__))
-
-
 app = Flask('drafthub')
+state = None
 
-@app.route('/', methods=['GET', 'POST'])
-def drafthub():
+QUERY_DRAFT_RESULTS = "select draft_results from fantasysports.draftresults where league_key='{}';"
+QUERY_PLAYER_KEYS = "select * from fantasysports.players where player_key in ({});";
+QUERY_TEAMS = "select * from fantasysports.teams where league_key='{}';"
+
+
+class DraftState(object):
+    def __init__(self, league_key, credentials='credentials.json'):
+        oauth = OAuth1(None, None, from_file=credentials)
+        self.yql = MYQL(format='json', oauth=oauth)
+
+        self.last_pick = 0
+        self.league_key = league_key
+
+        self.teams = self._query(QUERY_TEAMS.format(self.league_key))['team']
+        self.teams = { t['team_key']: t['name'] for t in self.teams }
+        print(json.dumps(self.teams, indent=2))
+
+        self.rosters = { name: [] for name in self.teams.values() }
+
+    def _query(self, query):
+        response = self.yql.raw_query(query)
+        data = json.loads(response.content)
+        return data['query']['results']
+
+    def refresh(self):
+        # TODO exclude known picks from query
+        picks = self._query(QUERY_DRAFT_RESULTS.format(self.league_key))['league']['draft_results']['draft_result'][self.last_pick:]
+        player_keys = []
+        for p in picks:
+            if 'player_key' in p and p['player_key']:
+                player_keys.append("'{}'".format(p['player_key']))
+            else:
+                self.last_pick = int(p['pick']) - 1
+                break
+
+        players = []
+
+        if player_keys:
+            players = self._query(QUERY_PLAYER_KEYS.format(', '.join(player_keys)))['player']
+            if isinstance(players, dict):
+                players = [players]
+
+            players = [p['name']['full'] for p in players]
+
+        return jsonify(picks=players)
+
+
+@app.route('/')
+def index():
+    return render_template('upload.html')
+
+
+@app.route('/draft', methods=['GET', 'POST'])
+def draft():
     if request.method == 'POST':
         reader = csv.reader(request.files['csv_path'])
         lines = []
         for row in reader:
             lines.append(row)
+
+        if request.form['game'] and request.form['league_id']:
+            global state
+            league_key = '{}.l.{}'.format(request.form['game'], request.form['league_id'])
+            state = DraftState(league_key)
 
         header = lines.pop(0)
         for i, td in enumerate(header):
@@ -35,6 +98,14 @@ def drafthub():
         }
         return render_template('draft.html', **context)
     return render_template('upload.html')
+
+
+@app.route('/_refresh')
+def refresh():
+    global state
+    if state:
+        return state.refresh()
+    return ''
 
 
 if __name__ == '__main__':
--- drafthub/static/js/drafthub.js
+++ drafthub/static/js/drafthub.js
@@ -1,33 +1,64 @@
 var drafted = [];
+var positions = {
+    'All': ['C', 'LW', 'RW', 'D', 'G'],
+    'Forward': ['C', 'LW', 'RW'],
+    'Defense': ['D'],
+    'Goalie': ['G']
+};
 
 function draft(player) {
-    drafted.push(player);
     player.className = 'taken ' + player.className;
 
     $('#current-pick').html(parseInt($('#current-pick').html(), 10) + 1);
 }
 
-function undo() {
-    if (drafted.length == 0) {
-        return;
-    }
-    var player = drafted.pop();
-    player.className = player.className.replace('taken', '');
-
-    $('#current-pick').html(parseInt($('#current-pick').html(), 10) - 1);
-}
-
-$('#player-search').bind('input propertychange', function() {
-    var search = $(this).val();
-
-    $('td.name').each(function(index, node) {
-        var name = node.innerHTML;
-        var parentNode = node.parentNode;
-        
-        if (!search || name.indexOf(search) != -1) {
-            parentNode.className = parentNode.className.replace('filter', '');
-        } else if (name.indexOf(search) == -1 && parentNode.className.indexOf('filter') == -1) {
-            parentNode.className = 'filter ' + parentNode.className;
-        }
+$(function() {
+    $('.player').bind('click', function() {
+        drafted.push(this);
+        draft(this);
     });
-})
+
+    $('#search').bind('input propertychange', function() {
+        var search = $(this).val();
+
+        $('td.name').each(function(index, node) {
+            var name = node.innerHTML;
+            var parentNode = node.parentNode;
+
+            if (!search || name.indexOf(search) != -1) {
+                parentNode.className = parentNode.className.replace('filter', '');
+            } else if (name.indexOf(search) == -1 && parentNode.className.indexOf('filter') == -1) {
+                parentNode.className = 'filter ' + parentNode.className;
+            }
+        });
+    });
+
+    $('#refresh').bind('click', function() {
+        $.getJSON($SCRIPT_ROOT + '/_refresh', null, function(data) {
+            var picks = data['picks'];
+            console.log(picks);
+            $('td.name').each(function(index, node) {
+                var name = node.innerHTML;
+
+                if (picks.indexOf(name) > -1) {
+                    draft(node.parentNode);
+                }
+            });
+        });
+    });
+
+    $('#undo').bind('click', function() {
+        if (drafted.length == 0) {
+            return;
+        }
+        var player = drafted.pop();
+        player.className = player.className.replace('taken', '');
+
+    });
+
+    $('#position > li > span').bind('click', function() {
+        var names = positions[this.innerHTML];
+
+
+    });
+});
